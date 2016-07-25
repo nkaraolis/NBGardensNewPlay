6665ab0423ac9769d12541625223c609801491f1
Yuan ZHANG <yuanzhang910@hotmail.com>
2016-07-25 12:21:18 +0100

    a bug on cart fixed

--- app/controllers/ProductPageController.scala
+++ app/controllers/ProductPageController.scala
@@ -1,44 +1,16 @@
 package controllers
 
-import play.api.mvc._
-import models.{Cart, Product}
 import play.api.mvc.{Action, Controller}
 import models.Product
 
+/**
+  * Created by Administrator on 08/07/2016.
+  */
 
 class ProductPageController extends Controller {
-
   def goToProduct(product: String) = Action {
     implicit request =>  //controller action
-
-      var clickedProduct = Product.findProductByName(product).get
-
+      val clickedProduct = Product.findProductByName(product).get
       Ok(views.html.productPage(clickedProduct))
-
   }
-
-
-  /*def ReviewData(review: String) {
-    val reviewForm = Form(
-      mapping(
-        "review" -> nonEmptyText.verifying("Review can not be left empty")
-      )(ReviewData.apply)(ReviewData.unapply)
-    )
-  }*/
-
-
-
-  /**var products: Array[Product] = Array.empty
-    **
-    *def add(product: String, Qty: String) = Action {
-    *implicit request =>  //controller action
-    *products :+ Cart.addToCart(Product.findByName(product).get)  //get product from model
-    *Redirect(routes.BrowseController.list) //render view template
-    * }
-    **
-    *def remove(product: String) = Action {
-    *implicit request =>  //controller action
-    *products = Cart.removeFromCart(Product.findByName(product).get)   //get product from model
-    *Ok(views.html.cartpage(products.toList)) //render view template
-    *}**/
 }
