883372b5a2ac2a2754a38568aa98c5483580fa0c
ZDevelop94 <hj310@greenwich.ac.uk>
2016-07-27 10:24:15 +0100

    Latest 2

--- app/controllers/OrderHistoryController.scala
+++ app/controllers/OrderHistoryController.scala
@@ -14,14 +14,43 @@ import play.api.Play.current
 import play.api.data.Forms.{longNumber, mapping, nonEmptyText}
 import play.api.i18n.Messages.Implicits._
 import views.html.helper.form
+import models.{Order}
 /**
   * Created by Administrator on 25/07/2016.
   */
 class OrderHistoryController @Inject() extends Controller {
 
   //point of entry to History page
-  def newPage = Action {
+  def newPage() = Action {
     implicit request =>
-      Ok(views.html.history(writeOrders.findOrders))
+      Ok(views.html.history(Order.getOrdersByCusId(cid)))
   }
+
+
+
+  //point of entry to History page
+  def newPage2(cid:String) = Action {
+    implicit request =>
+      Ok(views.html.history(Order.getOrdersByCusId(cid)))
+  }
+
+
+
+
+  //method to display an order's details
+  def track(id:Int) = Action {
+    implicit request =>
+      Order.findOrderById(id).map { order =>
+        Ok(views.html.trackOrder(order))
+      }.getOrElse(NotFound)
+
+  }
+
+
+
+
+
+
 }
+
+
--- app/models/DataDump.scala
+++ app/models/DataDump.scala
@@ -6,7 +6,28 @@ package models
   */
 object DataDump {
 
+  //some products
+  var prods: Array[Product] = new Array[Product](4)
+  prods(0) = Product("0001","Paperclips Large","Large Plain Pack of 1000", "100", "images/page3_img1.jpg", "images/big1.jpg", "", "Lawnmower")
+  prods(1) = Product("0002","Giant Paperclips","Giant Plain 51mm 100 pack", "100", "images/page3_img2.jpg", "images/big2.jpg", "", "Lawnmower")
+  prods(2) = Product("0003","Paperclip Giant Plain", "Giant Plain Pack of 10000", "100", "images/page3_img3.jpg", "images/big3.jpg", "", "Lawnmower")
+  prods(3) = Product("0004","No Tear Paper Clip", "No Tear Extra Large Pack of 1000", "100", "images/page3_img4.jpg", "images/big4.jpg", "", "Barbecues")
 
+
+
+  //a cart
+  var cart1: Cart = new Cart(prods)
+  var c1: Array[Cart] = new Array[Cart](1)
+  c1(0) = cart1
+
+
+
+  //an order
+  var o1 = Order(1, "punks", c1, "26 JUL 16, 15:23", "Order Made", "Pay Now")
+
+
+  //set of orders
+  var orders = List(o1)
 
 
 }
