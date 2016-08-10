11b062dd5d32ba71f7da9d870b7bca5857cc2091
ZDevelop94 <hj310@greenwich.ac.uk>
2016-08-10 09:28:52 +0100

    Refactored code, deleted unused classes

--- app/controllers/PayController.scala
+++ app/controllers/PayController.scala
@@ -9,7 +9,6 @@ import controllers._
 import play.api.i18n.Messages
 import scala.concurrent.ExecutionContext.Implicits.global
 import controllers.CartController
-import models.{CustomerCardDB, PaymentDetails}
 import play.api.data.format.Formats._
 
 
@@ -18,61 +17,12 @@ import play.api.data.format.Formats._
   */
 class PayController extends Controller {
 
-  //val items = Cart.findAllInCart  //get product from model
-
    //a payment form for the customer to select a previously saved card and a payment method (Pay Now or Pay Later)
-  /**val PayDetailsForm: Form[PaymentDetails] = Form(
-    mapping(
-      "Card" -> of[String],
-      "Method" -> of[String],
-      "Items" -> of[String],
-      "Total" -> of[Double]
-    )
-    (PaymentDetails.apply)
-    (PaymentDetails.unapply)
-  )**/
-
   val PayDetailsForm = Form(tuple(
     "Method" -> of[String],
     "Card" -> of[String]
 
   ))
-
-
-//  val PayDetailsForm = Form(tuple(
-//    "Card" -> of[String],
-//    "Method" -> of[String],
-//    "Items" -> of[String],
-//    "Total" -> of[Double]))
-
-//  // a form contents information about a card for the user to pay
-//  val CardForm: Form[CardDetails] = Form(
-//    mapping(
-//      "Payment Method" -> nonEmptyText,
-//      "Name on Card" -> nonEmptyText,
-//      "Card No" -> nonEmptyText,
-//      "Start Date" -> nonEmptyText,
-//      "Expiry Date" -> nonEmptyText,
-//      "Security Code" -> of[String],
-//      "Issue No" -> of[String]
-//    )
-//    (CardDetails.apply)
-//    (CardDetails.unapply)
-//  )
-
-
-
-//  def options = Action{
-//    implicit request =>
-//      val value = CardForm.bindFromRequest.data("Payment Method")
-//      if(value.equals("Paypal")) {
-//        val categories = Category.findAll
-//        Ok(views.html.browseCat(categories))
-//      }
-//      else{
-//        Ok("")
-//      }
-//  }
 
 
 
@@ -95,16 +45,12 @@ class PayController extends Controller {
       val payForm = PayDetailsForm.bindFromRequest()
       payForm.fold(success = {
         PayDetails =>
-       //val cardNumber = payForm.get._1
-       val method = payForm.get._1
-      //val newCard = CardDetails(newCardForm.get.method, newCardForm.data("Name on Card"), newCardForm.data("Card No"), newCardForm.data("Start Date"), newCardForm.data("Expiry Date"), newCardForm.data("Security Code"), newCardForm.data("Issue No"))
-      //val selectedCard = CustomerCardDB.findCard(cardId).get
+      val method = payForm.get._1
       val newID = OrderDB.findNextID()
       val status = "Order Made"
       val datetime = OrderDB.getDateTime()
       var order = new OrderDB(newID, username, Cart.productsInCart, total, datetime, status, cardNo, payForm.get._1)
       MongoConnector.collectionOrder.insert(order)
-      //CardDetails.add(newCard)
       //Empty Cart
       Cart.clearCart()
           Redirect(routes.BrowseController.categoryList())
